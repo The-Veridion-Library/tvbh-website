792eae9d8b862e6fdcf1ecae6866d961b4f1c710
TheAwesomeAJ <theawesomeaj.dev@gmail.com>
2026-01-12 21:49:29 -0500

    Added core files

---
+++ main/app/page.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+import { ArrowUpRight } from "lucide-react";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import Image from "next/image"
+
+export default function HeroSection() {
+    return (
+        <section className="flex min-h-screen items-center justify-center -mt-12 py-10 lg:py-16">
+            <div className="mx-auto max-w-7xl px-4">
+                <div className="grid items-center gap-8 lg:grid-cols-2">
+                </div>
+            </div>
+        </section>
+    )
+}
